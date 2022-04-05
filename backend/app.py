5f789d6034d495056a7a3355cbf9964b55d80e3e
Justin Sayah <55928969+justin-sayah@users.noreply.github.com>
2022-04-05 00:11:01 -0400

    testing to weather backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
@@ -6,7 +7,15 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+'''
+Stuff
+'''
 @app.route("/weather", methods=["GET"])
 def get_weather():
-    zip = request.values.get('zip')
-    return {"response": zip}
+
+    # lat = request.values.get("lat")
+    # long = request.values.get("long")
+    # location_request = "https://api.weather.gov/points/"+ lat + "," + long
+    location_point = requests.get("https://api.weather.gov/gridpoints/LWX/96,70/forecast", headers={"User-Agent": "(test.com, test@test.com)"})
+    #print(location_point)
+    return location_point
