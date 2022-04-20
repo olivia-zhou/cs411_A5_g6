74e658fd653f62278f85e32ebe1994a65d4f543c
Yvonne Wu <99497716+yvowu229@users.noreply.github.com>
2022-04-20 05:45:15 -0400

    map initial

---
+++ frontend/src/Map.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {GoogleMap, useLoadScript} from "@react-google-maps/api";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+    height: "50vh",
+    width: "50vw"
+};
+
+const center = {
+    lat: 42.3497644,
+    lng: -71.1041491
+};
+
+function Map() {
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries
+    });
+
+    if (loadError) return "Error";
+    if (!isLoaded) return "Loading...";
+
+    return (
+        <div>
+            <p>Google Map</p>
+            <br/>
+            <GoogleMap
+                id="map"
+                mapContainerStyle={mapContainerStyle}
+                zoom={8}
+                center={center}
+            >
+            </GoogleMap>
+        </div>
+    );
+}
+
+export default React.memo(Map);
