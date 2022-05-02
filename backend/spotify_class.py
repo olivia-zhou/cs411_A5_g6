3891b768d7718d78bcc29b95c036550d15b53fc5
Justin Sayah <55928969+justin-sayah@users.noreply.github.com>
2022-05-02 18:06:04 -0400

    spotify working

--- backend/spotify_class.py
+++ backend/spotify_class.py
@@ -13,7 +13,8 @@ class spotify:
         self.token = token
         #sentiment is passed in range of -1 to 1, but must be converted to range 0 to 1
         self.sentiment = (sentiment + 1)/2
-        self.country, self.username, self.email = self.get_me()
+        #self.country, self.username, self.email = self.get_me()
+        self.username = self.get_me()
 
         #these need to be replaced with one function that does everything
         self.playlist_url, self.playlist_id = self.create_empty_playlist()
@@ -24,12 +25,13 @@ class spotify:
     
     def get_me(self):
         info_url = 'https://api.spotify.com/v1/me'
-        userinfo = requests.get(info_url, headers={'Authorization': self.token})
-        response = json.loads(userinfo.text)
-        country = response['country']
-        name = response['name']
-        email= response['email']
-        return country, name, email
+        userinfo = requests.get(info_url, headers={'Authorization': "Bearer " + self.token})
+        response = userinfo.json()
+        #print(response)
+        #country = response['country']
+        name = response['display_name']
+        #email= response['email']
+        return name
     
     def create_empty_playlist(self):
         playlist_url = 'https://api.spotify.com/v1/users/{}/playlists'.format(self.username)
@@ -39,8 +41,8 @@ class spotify:
             'collaborative': False,
             'description': 'playlist generated based on the weather :)',
         }
-        create = requests.post(playlist_url, data = playlist_info, headers={'Authorization': self.token})
-        playlist = json.loads(create.text)
+        create = requests.post(playlist_url, json = playlist_info, headers={'Authorization': "Bearer " + self.token})
+        playlist = create.json()
         playlist_url = playlist['uri']
         playlist_id = playlist['id']
         return playlist_url, playlist_id
@@ -50,24 +52,25 @@ class spotify:
         seedGenres = random.sample(genres, k=5)
         seedGenresStr = ""
         for i in seedGenres:
-            seedGenres += i + ','
+            seedGenresStr += i + ','
         seedGenresStr = seedGenresStr[0:-1]
-        recom_url = 'https://api.spotify.com/v1/recommendations'
-        query_info = {
-            'seed_genres': seedGenresStr,
-            'limit': 50,
-            'target_valence': self.sentiment,
-            'min_populatity': 60,
-            'max_popularity':100,
-            'target_popularity':90
-        }
-        recs = requests.get(recom_url, data = query_info, headers={'Authorization': self.token})
-        response = json.loads(recs.text)
+        print(seedGenresStr)
+        the_url = "https://api.spotify.com/v1/recommendations?seed_genres={}&limit=50&target_valence={}&min_popularity=40&max_popularity=80&target_popularity=90".format(seedGenresStr, self.sentiment)
+        # query_info = {
+        #     'seed_genres': seedGenresStr,
+        #     'limit': 50,
+        #     'target_valence': self.sentiment,
+        #     'min_popularity': 60,
+        #     'max_popularity': 100,
+        #     'target_popularity': 90
+        # }
+        #recs = requests.get(recom_url, data = query_info, headers={'Authorization': "Bearer " + self.token})
+        recs = requests.get(the_url, headers={'Authorization': "Bearer " + self.token})
+        response = recs.json()
         tracks = response['tracks']
         track_uris = []
         for track in tracks:
             track_uris.append(track["uri"])
-
         self.song_list = track_uris
         self.add_music()
         return
@@ -84,10 +87,14 @@ class spotify:
     def add_music(self):
         music_url = 'https://api.spotify.com/v1/playlists/{}/tracks'.format(self.playlist_id)
         songs = {'uris': self.song_list}
-        add = requests.post(music_url, data = songs, headers={'Authorization': self.token})
-        response = json.loads(add.text)
+        add = requests.post(music_url, json = songs, headers={'Authorization': "Bearer " + self.token})
+        response = add.json()
         snapshot_id = response['snapshot_id']
         return snapshot_id
     
     def final_return(self):
         return self.playlist_url, self.playlist_id
+
+# test = spotify(1, 
+# 'BQA9ME8apfXBTi5RGZ6aDfarGTGIi5GZXbkTjPvBgkYFMrYKobNznBL_cbL0FRBU_fHTJIFJiqxA5x02VcYgoR_zpgSP38jMXua1EXYKiuwqivAcHsRhZlzvDNLaIWlYWsrQb8OtmGG8W_W7UezBmetiG7uv9Koucx9hRD-vPUN5nlpkJO2ntzmz-Ue8NR37QisBo3ZpsYa_g1-wfXHaF9XuVFQLW9Uo0LXO8OD4jLqM65THikL00Qs_a7DETgO1i3C91O5sHlnu8XJjChiZtxwH'
+# , -.97)
